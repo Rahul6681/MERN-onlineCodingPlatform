0261c26d5b6f0025484e4b954be2a3cb86c15deb
Rahul Pandit <rpandit6566@gmail.com>
2026-07-21 21:05:11 +0530

    Fix Vercel serverless premature termination by exporting Express app directly in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-const connectDB = require('../backend/src/config/db');
 const { app } = require('../backend/src/server');
 
-module.exports = async (req, res) => {
-  try {
-    await connectDB();
-  } catch (err) {
-    console.error('[Vercel Serverless DB Error]:', err.message);
-  }
-  return app(req, res);
-};
+module.exports = app;
